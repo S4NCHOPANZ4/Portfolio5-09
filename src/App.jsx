08d9d7737650e1098c9a369ba01598e525c09d05
Juan Buitrago <buitr4go@gmail.com>
2023-07-18 00:37:02 -0700

    fixed styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ParallaxC from "./Parallax";
 
 function App() {
+
+  const [loaded, setLoaded] = useState(false)
+
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoaded(true)
+    };
+    window.onload = handleLoad;
+    return () => {
+      window.onload = null;
+    };
+  }, []);
+
+
   return (
     <>
+      {loaded?
       <ParallaxC />
+      :
+
+      <div className="loader_super">
+        <div className="loader"></div>
+      </div>
+      }
+      
     </>
   );
 }
