c3333d982a4bc139c393ae4936282c831f631528
Juan Buitrago <buitr4go@gmail.com>
2023-08-23 19:20:14 -0700

    new project display model

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,13 @@ import React from "react";
 //img
 import "../styles/projects/index.scss";
 import AnimatedText from "../components/AnimatedText";
+
+import filmFoliaImg from "../assets/svg-p/FilmFolia.png";
+import stereoHeartsImg from "../assets/svg-p/stereo.png";
+import newEggImg from "../assets/svg-p/newegg.png";
+import pomodorosImg from "../assets/svg-p/pomofocus.png";
+import loltrackerImg from "../assets/svg-p/leaguetraker.png";
+
 import newEggIcon from "../assets/project-icons/newEggIcon.png";
 import filmFolia from "../assets/project-icons/film-folia-icon.png";
 import leagueTrackerIcon from "../assets/project-icons/lt.png";
@@ -23,7 +30,7 @@ import { FaCcStripe, FaCcPaypal } from "react-icons/fa";
 import { RxOpenInNewWindow } from "react-icons/rx";
 import { AiFillGithub } from "react-icons/ai";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode, Pagination, Navigation } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -36,11 +43,11 @@ const breakpoints = {
     spaceBetween: 20,
   },
   800: {
-    slidesPerView: 2,
+    slidesPerView: 1.6,
     spaceBetween: 40,
   },
   1324: {
-    slidesPerView: 3,
+    slidesPerView: 2.6,
     spaceBetween: 50,
   },
 };
@@ -50,34 +57,73 @@ const Projects = () => {
     {
       demo: "film-folia",
       github: "S4NCHOPANZ4/FilmFolia",
+      title: "Film Folia",
+      description:
+        "React App that seamlessly merges MongoDB, Tailwind CSS, Node.js and Express, creating a space where movie fans around the world can share their thoughts and discussions.",
+      icon: filmFolia,
+      background: filmFoliaImg,
+      toolkit: [
+        "BiLogoJavascript",
+        "BiLogoReact",
+        "TbApi",
+        "BiLogoTailwindCss",
+        "BiLogoMongodb",
+        "GrNode",
+      ],
     },
     {
       demo: "stereo-heart",
       github: "S4NCHOPANZ4/StereoHeart",
+      title: "Stereo Hearts",
+      description:
+        "Combining the power of React, JavaScript, and Tailwind, we harness the Shazam Core API to craft an immersive audio discovery platform. Elevate your music experience and explore tracks seamlessly, redefining audio exploration, just like Spotify.",
+      icon: stereoIcon,
+      background: stereoHeartsImg,
+      toolkit: [
+        "BiLogoJavascript",
+        "BiLogoReact",
+        "BiLogoTailwindCss",
+        "TbApi",
+      ],
     },
     {
       demo: "new-egg-ecomerce-y9so",
       github: "S4NCHOPANZ4/newEgg_Ecomerce/tree/main",
+      title: "New Egg",
+      description:
+        "React e-commerce app. Shop securely with PayPal and Stripe. Discover a wide range of dummy data products as we work to perfect the shopping destination (under development).",
+      icon: newEggIcon,
+      background: newEggImg,
+      toolkit: [
+        "BiLogoReact",
+        "GrNode",
+        "BiLogoTailwindCss",
+        "BiLogoMongodb",
+        "FaCcStripe",
+        "FaCcPaypal",
+      ],
     },
     {
       demo: "pomodoros-puce",
       github: "S4NCHOPANZ4/pomodoros",
+      title: "Pomofocus",
+      description:
+        "Boost your productivity and stay focused with this React app inspired by the Pomodoro Technique",
+      icon: pomodoroIcon,
+      background: pomodorosImg,
+      toolkit: ["BiLogoReact", "BiLogoTypescript", "FaSass"],
     },
     {
       demo: "lol-tracker-v1",
       github: "S4NCHOPANZ4/RiotAPi",
+      title: "LeagueTracker",
+      description:
+        "React app utilizing the Riot Games API to explore match information, discover top players on different servers, and view your own or your friends' matches.",
+      icon: leagueTrackerIcon,
+      background: loltrackerImg,
+      toolkit: ["BiLogoReact", "BiLogoTypescript", "FaSass", "TbApi"],
     },
   ];
-
-  const goToDemo = (deployment, urlplus) => {
-    window.open(
-      `https://${deployment}.vercel.app${urlplus ? urlplus : ""}`,
-      "_blank"
-    );
-  };
-  const goToGitHub = (deployment) => {
-    window.open(`https://github.com/${deployment}`, "_blank");
-  };
 
   return (
     <div className="projects__super">
@@ -89,175 +135,92 @@ const Projects = () => {
         </div>
       </div>
       <div className="swiper-cont">
-        <GoArrowLeft className="swiper-icon" size={20} />
         <Swiper
+          spaceBetween={20}
           slidesPerView={1}
           freeMode={true}
-          modules={[FreeMode]}
+          modules={[Navigation]}
+          navigation
           className="mySwiper"
           breakpoints={breakpoints}
         >
-          <SwiperSlide className="swiper-slide">
-            <div 
-            className="project_card project_card_1">
-              <div className="project_card_icon">
-                <img src={filmFolia} alt="" />
-              </div>
-              <div className="project_desc">
-                <p>
-                  React App that seamlessly merges MongoDB, Tailwind CSS,
-                  Node.js and Express, creating a space where movie fans around
-                  the world can share their thoughts and discussions.
-                </p>
-                <div className="project_links">
-                  <RxOpenInNewWindow
-                    onClick={() => goToDemo(projects[0].demo)}
-                    size={20}
-                  />
-                  <AiFillGithub
-                    onClick={() => goToGitHub(projects[0].github)}
-                    size={20}
-                  />
-                </div>
-              </div>
-              <div className="toolkits">
-                <BiLogoJavascript className="tools" size={35} />
-                <BiLogoReact className="tools" size={35} />
-                <TbApi className="tools" size={35} />
-                <BiLogoTailwindCss className="tools" size={35} />
-                <BiLogoMongodb className="tools" size={35} />
-                <GrNode className="tools" size={35} />
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide custom-width-slide">
-            <div 
-            className="project_card project_card_2">
-
-              <div className="project_card_icon">
-                <img src={stereoIcon} alt="" />
-              </div>
-              <div className="project_desc">
-                <p>
-                  Combining the power of React, JavaScript, and Tailwind, we
-                  harness the Shazam Core API to craft an immersive audio
-                  discovery platform. Elevate your music experience and explore
-                  tracks seamlessly, redefining audio exploration, just like
-                  Spotify.
-                </p>
-                <div className="project_links">
-                  <RxOpenInNewWindow
-                    onClick={() => goToDemo(projects[1].demo)}
-                    size={20}
-                  />
-                  <AiFillGithub
-                    onClick={() => goToGitHub(projects[1].github)}
-                    size={20}
-                  />
-                </div>
-              </div>
-              <div className="toolkits">
-                <BiLogoJavascript className="tools" size={35} />
-                <BiLogoReact className="tools" size={35} />
-                <BiLogoTailwindCss className="tools" size={35} />
-                <TbApi className="tools" size={35} />
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide custom-width-slide">
-            <div 
-            className="project_card project_card_3">
-              <div className="project_card_icon">
-                <img src={newEggIcon} alt="" />
-              </div>
-              <div className="project_desc">
-                <p>
-                  React e-commerce app. Shop securely with PayPal and Stripe.
-                  Discover a wide range of dummy data products as we work to
-                  perfect the shopping destination (under development).
-                </p>
-                <div className="project_links">
-                  <RxOpenInNewWindow
-                    onClick={() => goToDemo(projects[2].demo)}
-                    size={20}
-                  />
-                  <AiFillGithub
-                    onClick={() => goToGitHub(projects[2].github)}
-                    size={20}
-                  />
-                </div>
-              </div>
-              <div className="toolkits">
-                <BiLogoReact className="tools" size={35} />
-                <GrNode className="tools" size={35} />
-                <BiLogoTailwindCss className="tools" size={35} />
-                <BiLogoMongodb className="tools" size={35} />
-                <FaCcStripe className="tools" size={35} />
-                <FaCcPaypal className="tools" size={35} />
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide custom-width-slide">
-            <div className="project_card project_card_4">
-              <div className="project_card_icon">
-                <img src={pomodoroIcon} alt="" />
-              </div>
-              <div className="project_desc">
-                <p>
-                  Boost your productivity and stay focused with this React app
-                  inspired by the Pomodoro Technique
-                </p>
-                <div className="project_links">
-                  <RxOpenInNewWindow
-                    onClick={() => goToDemo(projects[3].demo)}
-                    size={20}
-                  />
-                  <AiFillGithub
-                    onClick={() => goToGitHub(projects[3].github)}
-                    size={20}
-                  />
-                </div>
-              </div>
-              <div className="toolkits">
-                <BiLogoReact className="tools" size={35} />
-                <BiLogoTypescript className="tools" size={35} />
-                <FaSass className="tools" size={35} />
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide custom-width-slide">
-            <div className="project_card project_card_5"
-            >
-              <div className="project_card_icon">
-                <img src={leagueTrackerIcon} alt="" />
-              </div>
-              <div className="project_desc">
-                <p>
-                  React app utilizing the Riot Games API to explore match
-                  information, discover top players on different servers, and
-                  view your own or your friends' matches.
-                </p>
-                <div className="project_links">
-                  <RxOpenInNewWindow
-                    onClick={() => goToDemo(projects[4].demo)}
-                    size={20}
-                  />
-                  <AiFillGithub
-                    onClick={() => goToGitHub(projects[4].github)}
-                    size={20}
-                  />
-                </div>
-              </div>
-              <div className="toolkits">
-                <BiLogoReact className="tools" size={30} />
-                <BiLogoTypescript className="tools" size={30} />
-                <FaSass className="tools" size={30} />
-                <TbApi className="tools" size={35} />
-              </div>
+          {projects.map((element, index) => {
+            return (
+              <SwiperSlide key={element.demo} className="swiper-slide">
+                  <ProjectCard data={element} index={index} />
+              </SwiperSlide>
+            );
+          })}
+          <SwiperSlide>
+            <div className="comming-soon">
+              <p>Comming Soon!</p>
             </div>
           </SwiperSlide>
         </Swiper>
-        <GoArrowRight className="swiper-icon" size={20} />
+
+      </div>
+    </div>
+  );
+};
+
+const DynamicComponent = ({ componentString, index }) => {
+  const Component = eval(componentString);
+  return (
+    <>
+      <Component className="tools" size={35} key={componentString} />
+    </>
+  );
+};
+
+const ProjectCard = ({ data, index }) => {
+  const goToDemo = (deployment, urlplus) => {
+    window.open(
+      `https://${deployment}.vercel.app${urlplus ? urlplus : ""}`,
+      "_blank"
+    );
+  };
+  const goToGitHub = (deployment) => {
+    window.open(`https://github.com/${deployment}`, "_blank");
+  };
+
+  return (
+    <div key={index} className="project-container">
+      <div className="project-ss">
+        <div className="main-project-info">
+          <p>{data.title}</p>
+
+          <div className="nav-links">
+            <RxOpenInNewWindow
+              className="icon-1"
+              onClick={() => goToDemo(data.demo)}
+              size={25}
+            />
+            <AiFillGithub
+              className="icon-2"
+              onClick={() => goToGitHub(data.github)}
+              size={25}
+            />
+          </div>
+        </div>
+        <div className="light-cover"></div>
+        <img src={data.background} alt="" />
+      </div>
+      <div className="project-info">
+        <div className="project-desc">
+          <p>{data.description}</p>
+        </div>
+        <div className="project-icon">
+          <img src={data.icon} alt="" />
+        </div>
+        <div className="project-toolkit">
+          <div className="toolkits">
+            {data.toolkit.map((tool, i) => {
+              return (
+                <DynamicComponent index={i} key={i} componentString={data.toolkit[i]} />
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
